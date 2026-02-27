6416326c072860a0ffed249b96a07576f66e422d
Kovalenko Valeria <VKavalenka@ibagroup.eu>
2026-02-27 19:42:50 +0300

    Подправила структуру

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,4 +1,5 @@
-import { useGetBooksQuery } from "../src/api/api.js";
+import { useGetBooksQuery } from "../src/api/api.js"
+import Header from "./components/Header/Header.jsx";
 
 function App() {
   const { data: books = [], isLoading, isError } = useGetBooksQuery();
@@ -7,24 +8,28 @@ function App() {
   if (isError) return <p>Ошибка загрузки</p>;
 
   return (
-    <div>
-      <h1>Книжный магазин</h1>
-      <ul>
-        {books.map(book => (
-          <li key={book.id} style={{ marginBottom: "20px" }}>
-            <h2>{book.title}</h2>
-            <p>Автор: {book.author}</p>
-            <p>Цена: {book.price} ₽</p>
-            <img
-              src={book.imageUrl}
-              alt={book.title}
-              style={{ width: "150px", height: "220px", objectFit: "cover" }}
-            />
-            <div>{book.description}</div>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+        <Header />
+
+        <div>
+          <h1>Книжный магазин</h1>
+          <ul>
+            {books.map(book => (
+              <li key={book.id} style={{ marginBottom: "20px" }}>
+                <h2>{book.title}</h2>
+                <p>Автор: {book.author}</p>
+                <p>Цена: {book.price} ₽</p>
+                <img
+                  src={book.imageUrl}
+                  alt={book.title}
+                  style={{ width: "150px", height: "220px", objectFit: "cover" }}
+                />
+                <div>{book.description}</div>
+              </li>
+            ))}
+          </ul>
+        </div>
+    </>
   );
 }
 
