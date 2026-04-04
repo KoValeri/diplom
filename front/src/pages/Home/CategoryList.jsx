a92746ad7581882a482acec19ba7236857316edd
Kovalenko Valeria <VKavalenka@ibagroup.eu>
2026-04-04 19:01:11 +0300

    Добавлен эффект переворота

--- front/src/pages/Home/CategoryList.jsx
+++ front/src/pages/Home/CategoryList.jsx
@@ -26,32 +26,35 @@ export default function CategoryList({ categories }) {
         };
     }, []);
 
-    return(
+    return (
         <div>
             <div className={styles.categories}>
                 {categories.map((cat) => (
-                    <div className={styles.category}>
-                        {openCategoryId === cat.id ? (
-                            <>
-                                <div className={styles.subcategories}>
-                                    <ul>
-                                        {cat.subcategories.map(sub => (
-                                            <li key={sub.id}>
-                                                <Link to={ROUTES.BOOKSBYCATEGORIES.replace(':id', sub.id)}>
-                                                    {sub.name}
-                                                </Link>
-                                            </li>
-                                        ))}
-                                    </ul>
+                    <div className={styles.category} key={cat.id}>
+                        <div className={styles.posterWrapper}>
+                            <div
+                                className={`${styles.posterCard} ${openCategoryId === cat.id ? styles.flipped : ""}`}
+                                onClick={() => toggleSubCategories(cat.id)}
+                            >
+                                <img className={styles.posterFront} src={cat.poster} alt={cat.name} />
+
+                                <div className={styles.posterBack}>
+                                    <div className={styles.subcategories}>
+                                        <ul>
+                                            {cat.subcategories.map(sub => (
+                                                <li key={sub.id}>
+                                                    <Link to={ROUTES.BOOKSBYCATEGORIES.replace(':id', sub.id)}>
+                                                        {sub.name}
+                                                    </Link>
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </div>
                                 </div>
-                                <div className={styles.floorMenu}></div>
-                            </>
-                        ) : (
-                            <div className={styles.posterWrapper}>
-                                <img className={styles.poster} src={cat.poster} alt={cat.name} onClick={() => toggleSubCategories(cat.id)}/>
-                                <div className={styles.floor}></div>
                             </div>
-                        )}
+                            <div className={styles.floor}></div>
+                        </div>
+
                         <div className={styles.categoryName} onClick={() => toggleSubCategories(cat.id)}>
                             {cat.name}
                         </div>
